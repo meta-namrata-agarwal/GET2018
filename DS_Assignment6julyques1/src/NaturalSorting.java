571527436f7c65f4504f264b7726036294607b77
NAMRATA AGARWAL <NAMRATA.AGARWAL@metacube.com>
2018-08-08 18:35:36 +0530

    employee

---
+++ DS_Assignment6julyques1/src/NaturalSorting.java
@@ -0,0 +1,22 @@
+import java.util.Comparator;
+
+/**
+ * class to sort the list by employee id
+ * 
+ * @author Namrata Agarwal
+ *
+ */
+public class NaturalSorting implements Comparator<Employee> {
+    /**
+     * method to sort the list of employees using unique id
+     */
+    @Override
+    public int compare(Employee employee1, Employee employee2) {
+        if (employee1.getempId() > ((employee2.getempId())))
+            return 1;
+        if (employee1.getempId() < ((employee2.getempId())))
+            return -1;
+        return 0;
+
+    }
+}
---
+++ DS_Assignment6julyques1/src/SortByName.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+
+/**
+ * class to sort the list by employee name
+ * 
+ * @author Namrata Agarwal
+ *
+ */
+public class SortByName implements Comparator<Employee> {
+    /**
+     * method to sort the list of employees using name by ascending order
+     */
+    @Override
+    public int compare(Employee employee1, Employee employee2) {
+
+        return employee1.getEmpName().compareTo(employee2.getEmpName());
+
+    }
+
+}
