fcbdda2c618e74065c992c7d3c4025b12672d991
meta-namrata-agarwal <41181679+meta-namrata-agarwal@users.noreply.github.com>
2018-08-13 11:32:40 +0530

    Update TestFile.java

--- DS_Assignment2ques5/src/TestFile.java
+++ DS_Assignment2ques5/src/TestFile.java
@@ -1,46 +1,39 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
 
-class TestFile {
+ public class TestFile {
 
     @Test
     public void testSum() {
-      List<Integer> list1=(Arrays.asList(3, 4, 6)); 
-      List<Integer> list2=new ArrayList<>(Arrays.asList(1, 6, 8));
-      List<Integer> list3=new ArrayList<>(Arrays.asList(3, 2, 9));
-      List<List<Long>> testList=new ArrayList<>();
-      testList.add(list1);
-      testList.add(list2);
-      testList.add(list3);
-      int expected=42;
-      NestedList nestedList=new NestedList(testList);
-      assertEquals(expected,nestedList.sumOfIntegerInList());
+    
     }
     
     @Test
     public void testLargest() {
-      List<Integer> list1=new ArrayList<>(Arrays.asList(3, 4, 6)); 
-      List<Integer> list2=new ArrayList<>(Arrays.asList(1, 6, 8));
-      List<Integer> list3=new ArrayList<>(Arrays.asList(3, 2, 9));
+      List<Integer> list1=new ArrayList<>(Arrays.asList(11, 4, 6)); 
+      List<Integer> list2=new ArrayList<>(Arrays.asList(1, 3, 8));
+      List<Integer> list3=new ArrayList<>(Arrays.asList(7, 2, 9));
       List<List<Integer>> testList=new ArrayList<>();
       testList.add(list1);
       testList.add(list2);
       testList.add(list3);
-      int expected=9;
+      int expected=11;
       NestedList nestedList=new NestedList(testList);
       assertEquals(expected, nestedList.largestValue());
     }
     
     @Test
     public void testSearchSuccessful() {
-      List<Integer> list1=new ArrayList<>(Arrays.asList(3, 4, 6)); 
-      List<Integer> list2=new ArrayList<>(Arrays.asList(1, 6, 8));
-      List<Integer> list3=new ArrayList<>(Arrays.asList(3, 2, 9));
+      List<Integer> list1=new ArrayList<>(Arrays.asList(11, 4, 6)); 
+      List<Integer> list2=new ArrayList<>(Arrays.asList(1, 3, 8));
+      List<Integer> list3=new ArrayList<>(Arrays.asList(7, 2, 9));
       List<List<Integer>> testList=new ArrayList<>();
       testList.add(list1);
       testList.add(list2);
@@ -51,9 +44,9 @@ class TestFile {
     
     @Test
     public void testSearchUnsuccessful() {
-      List<Integer> list1=new ArrayList<>(Arrays.asList(3, 4, 6)); 
-      List<Integer> list2=new ArrayList<>(Arrays.asList(1, 6, 8));
-      List<Integer> list3=new ArrayList<>(Arrays.asList(3, 2, 9));
+      List<Integer> list1=new ArrayList<>(Arrays.asList(11, 4, 6)); 
+      List<Integer> list2=new ArrayList<>(Arrays.asList(1, 3, 8));
+      List<Integer> list3=new ArrayList<>(Arrays.asList(7, 2, 9));
       List<List<Integer>> testList=new ArrayList<>();
       testList.add(list1);
       testList.add(list2);
@@ -62,4 +55,18 @@ class TestFile {
       assertEquals(false,nestedList.searchValue(10));
     }
 
+    @Test
+    public void testSumUnsuccessful() {
+      List<Integer> list1=new ArrayList<>(Arrays.asList(11, 4, 6)); 
+      List<Integer> list2=new ArrayList<>(Arrays.asList(1, 3, 8));
+      List<Integer> list3=new ArrayList<>(Arrays.asList(7, 2, 9));
+      List<List<Integer>> testList=new ArrayList<>();
+      testList.add(list1);
+      testList.add(list2);
+      testList.add(list3);
+      NestedList nestedList=new NestedList(testList);
+      int expected=51;
+      assertEquals(expected,nestedList.sumOfIntegerInList());
+    }
 }
+
