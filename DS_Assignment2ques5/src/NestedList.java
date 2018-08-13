ffb185753f0a13d226c2c13ad41c525bf295b9ab
meta-namrata-agarwal <41181679+meta-namrata-agarwal@users.noreply.github.com>
2018-08-13 11:30:31 +0530

    Update NestedList.java

--- DS_Assignment2ques5/src/NestedList.java
+++ DS_Assignment2ques5/src/NestedList.java
@@ -1,42 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
 
+import java.util.List;
+/**
+ * 
+ * @author User20
+ *
+ */
 public class NestedList implements NestedListInterface {
 
-    private List<List<Long>> nestedList;
+    private List<List<Integer>> nestedList;
 
-    public NestedList(List<List<Long>> nestedList2) {
+    public NestedList(List<List<Integer>> nestedList2) {
         this.nestedList = nestedList2;
     }
 
     @Override
-    public Long  sumOfIntegerInList() {
+    public int sumOfIntegerInList() {
         if (nestedList.isEmpty())
             throw new AssertionError();
-       
-        Long sum =  (long) 0;
+
+        int sum = 0;
         for (int i = 0; i < nestedList.size(); i++) {
             for (int j = 0; j < nestedList.get(i).size(); j++) {
-                sum=sum+nestedList.get(i).get(j);
-               // System.out.println(nestedList.get(i).get(j));
+                sum = sum + nestedList.get(i).get(j);
+
             }
-            
+
         }
-        
 
         return sum;
     }
 
     @Override
-    public Long largestValue() {
-        if(nestedList.isEmpty())
+    public int largestValue() {
+        if (nestedList.isEmpty())
             throw new AssertionError();
-        Long max=(long) 0;
+        int max = 0;
         for (int i = 0; i < nestedList.size(); i++) {
             for (int j = 0; j < nestedList.get(i).size(); j++) {
-                if(nestedList.get(i).get(j)>max)
-                {
-                    max=nestedList.get(i).get(j);
+                if (nestedList.get(i).get(j) > max) {
+                    max = nestedList.get(i).get(j);
                 }
             }
         }
@@ -46,14 +48,13 @@ public class NestedList implements NestedListInterface {
 
     @Override
     public boolean searchValue(int value) {
-        if(nestedList.isEmpty())
+        if (nestedList.isEmpty())
             throw new AssertionError();
-       
+
         for (int i = 0; i < nestedList.size(); i++) {
             for (int j = 0; j < nestedList.get(i).size(); j++) {
-                if(value==nestedList.get(i).get(j))
-                {
-                  return true;
+                if (value == nestedList.get(i).get(j)) {
+                    return true;
                 }
             }
         }
