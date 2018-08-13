c7b9e112cc64ea8f5328708517c5dfbb6105f8cc
meta-namrata-agarwal <41181679+meta-namrata-agarwal@users.noreply.github.com>
2018-08-13 11:29:47 +0530

    Update MainForJason.java

--- DS_Assignment2ques5/src/MainForJason.java
+++ DS_Assignment2ques5/src/MainForJason.java
@@ -4,30 +4,44 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+/**
+ * 
+ * @author User20
+ *
+ */
 public class MainForJason {
     public static void main(String[] args) {
 
         JSONParser jsonParser = new JSONParser();
         Object object;
+        List<List<Integer>> nestedList = new ArrayList<List<Integer>>();
 
         try {
 
-            object = jsonParser.parse(
-                    new FileReader("C:\\Users\\Admin\\eclipse-workspace\\DS_Assignment2ques5\\src\\input.json.txt"));
+            object = jsonParser
+                    .parse(new FileReader(
+                            "C:/Users/User20/workspace/DS_Assignment2ques5/jason.json.txt"));
             JSONObject jsonObject = (JSONObject) object;
-            List<List<Long>> nestedList = new ArrayList<List<Long>>();
-            for (int count = 0; count < 5; count++) {
-                @SuppressWarnings("unchecked")
-                List<Long> list = (List<Long>) jsonObject.get(String.valueOf(count));
+
+            for (Object jsonKey : jsonObject.keySet()) {
+
+                JSONArray jsonArray = (JSONArray) jsonObject.get(jsonKey);
+                List<Integer> list = new ArrayList<>();
+                for (Object value : jsonArray) {
+
+                    list.add(Integer.parseInt(value.toString()));
+
+                }
                 nestedList.add(list);
             }
             NestedList listAdd = new NestedList(nestedList);
             System.out.println(nestedList);
-            System.out.println(listAdd.searchValue(12));
+            System.out.println(listAdd.searchValue(2));
             System.out.println(listAdd.largestValue());
             System.out.println(listAdd.sumOfIntegerInList());
 
