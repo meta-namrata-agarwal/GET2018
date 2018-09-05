6f399e8f422fd5e5a485a16ca4cd185cc5319da7
NAMRATA AGARWAL <NAMRATA.AGARWAL@metacube.com>
2018-09-05 18:01:32 +0530

    ques1 and ques3

---
+++ SpringCollectionExample/src/com/metacube/training/collection/CollectionEntity.java
@@ -0,0 +1,48 @@
+package com.metacube.training.collection;
+
+import java.util.*;
+
+public class CollectionEntity {
+
+	private List nameList;
+	private Set nameSet;
+	private Map nameMap;
+	/**
+	 * @return the nameList
+	 */
+	public List getNameList() {
+		return nameList;
+	}
+	/**
+	 * @param nameList the nameList to set
+	 */
+	public void setNameList(List nameList) {
+		this.nameList = nameList;
+	}
+	/**
+	 * @return the nameSet
+	 */
+	public Set getNameSet() {
+		return nameSet;
+	}
+	/**
+	 * @param nameSet the nameSet to set
+	 */
+	public void setNameSet(Set nameSet) {
+		this.nameSet = nameSet;
+	}
+	/**
+	 * @return the nameMap
+	 */
+	public Map getNameMap() {
+		return nameMap;
+	}
+	/**
+	 * @param nameMap the nameMap to set
+	 */
+	public void setNameMap(Map nameMap) {
+		this.nameMap = nameMap;
+	}
+	
+	
+}
---
+++ SpringDemo/src/com/metacube/training/constructor/SpellChecker.java
@@ -0,0 +1,9 @@
+package com.metacube.training.constructor;
+
+public class SpellChecker {
+
+	public String spellCheck()
+	{
+		return "spell check";
+	}
+}
