b93abc6f1d5ec4dbda05fba76cfe7b448587b291
NAMRATA AGARWAL <NAMRATA.AGARWAL@metacube.com>
2018-09-05 18:18:57 +0530

    ques4

---
+++ SpringScopeExample/src/com/metacube/training/ScopeExample.java
@@ -0,0 +1,21 @@
+package com.metacube.training;
+
+public class ScopeExample {
+
+	private String message;
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
